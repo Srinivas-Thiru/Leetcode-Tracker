807e35aafae4ffb058cd6e2bb449ab6e7da3c30d
Srinivas T <110653801+Srinivas-Thiru@users.noreply.github.com>
2026-05-21 09:28:11 -0500

    Time: 23 ms (50.41%) | Memory: 14.2 MB (17.06%) - LeetSync

---
+++ Problems-Solutions/242-valid-anagram/valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        counts = {}
+        countt = {}
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            counts[s[i]] = counts.get(s[i], 0) + 1
+            countt[t[i]] = countt.get(t[i], 0) + 1
+        
+        return countt == counts
+        
+     
